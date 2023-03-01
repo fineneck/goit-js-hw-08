762e62586dca232b6da15b3c025764ee611a96ab
OleksiiMikhieiev <babybong0993@gmail.com>
2023-03-01 02:50:12 +0200

    add-gallery+simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const cardsMarkup = createImgCardsMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createImgCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image"
+      src="${preview}" 
+      alt="${description}" 
+      /></a>
+      `;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
